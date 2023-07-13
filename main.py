c69cf486ead106dfe5a50acbad7f89c2962e9cfa
Adrian <adiadrian9798@gmail.com>
2023-07-13 13:36:01 +0300

    test

--- main.py
+++ main.py
@@ -4,9 +4,9 @@
 from ultralytics import YOLO
 
 if __name__ == '__main__':
-    model = YOLO("runs/detect/train31/weights/best.pt") # 
+    model = YOLO("runs/detect/train38/weights/best.pt") # 
 
-    result = model.train(data = "config.yaml", epochs = 50, device = 0, amp = False, workers = 2)#amp = False, batch = 4, workers = 3
+    result = model.train(data = "config.yaml", epochs = 200, device = 0, amp = False, workers = 2)#amp = False, batch = 4, workers = 3
 
 #if __name__ == '__main__':
  #   result = main()
